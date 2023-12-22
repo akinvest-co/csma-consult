2f027d951c27ebd621c1555a96efa7ceb761bb96
nikuze <nikuzediop@gmail.com>
2023-12-22 17:54:37 +0200

    Responsive Design for Bureau/Solutions on home page

--- app/(home)/components/bureau-solutions/bureau-solutions.tsx
+++ app/(home)/components/bureau-solutions/bureau-solutions.tsx
@@ -10,56 +10,76 @@ import {
   Heading,
   Text,
   Link,
+  Box,
 } from "@chakra-ui/react";
 
 const BureauSolutions = () => {
   return (
     <Container maxW="container.lg" my={40}>
-      <Center height={40}>
-        <>
+      <Center height="100px">
+        <Box>
           {bureauData.map(({ title, description, linkText, path }) => (
-            <VStack align="start" spacing="8">
-              <Heading
-                size={{ base: "md", md: "lg" }}
-                fontWeight="bold"
-                textTransform="uppercase"
-              >
-                {title}
-              </Heading>
-              <Text>{description}</Text>
-              <Link href={path} color="#1799cf" _hover={{ textDecor: "none" }}>
-                <span>{linkText}</span>
-                <span style={{ marginLeft: "15px" }}>&rarr;</span>
-              </Link>
+            <VStack align="start" spacing="8" key={title}>
+              <Box>
+                <Heading
+                  size={{ base: "md", md: "md" }}
+                  fontWeight="bold"
+                  textTransform="uppercase"
+                >
+                  {title}
+                </Heading>
+              </Box>
+              <Box>
+                <Text>{description}</Text>
+              </Box>
+              <Box>
+                <Link
+                  href={path}
+                  color="#1799cf"
+                  _hover={{ textDecor: "none" }}
+                >
+                  <span>{linkText}</span>
+                  <span style={{ marginLeft: "15px" }}>&rarr;</span>
+                </Link>
+              </Box>
             </VStack>
           ))}
-        </>
+        </Box>
 
         <Divider
           orientation="vertical"
           mx={10}
           borderColor="#1799cf"
-          borderWidth={1}
-          height="100%"
+          borderWidth={2}
         />
-        <>
+        <Box>
           {solutionsData.map(({ title, description, linkText, path }) => (
-            <VStack align="start" spacing="8">
-              <Heading
-                size={{ base: "md", md: "lg" }}
-                fontWeight="bold"
-                textTransform="uppercase"
-              >
-                {title}
-              </Heading>
-              <Text>{description}</Text>
-              <Link href={path} color="#1799cf" _hover={{ textDecor: "none" }}>
-                <span>{linkText}</span>
-                <span style={{ marginLeft: "15px" }}>&rarr;</span>
-              </Link>
+            <VStack align="start" spacing="8" key={title}>
+              <Box>
+                <Heading
+                  size={{ base: "md", md: "md" }}
+                  fontWeight="bold"
+                  textTransform="uppercase"
+                >
+                  {title}
+                </Heading>
+              </Box>
+              <Box>
+                <Text>{description}</Text>
+              </Box>
+              <Box>
+                <Link
+                  href={path}
+                  color="#1799cf"
+                  _hover={{ textDecor: "none" }}
+                >
+                  <span>{linkText}</span>
+                  <span style={{ marginLeft: "15px" }}>&rarr;</span>
+                </Link>
+              </Box>
             </VStack>
           ))}
-        </>
+        </Box>
       </Center>
     </Container>
   );
