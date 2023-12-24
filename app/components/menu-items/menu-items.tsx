3416570edd12b6915c56eb28da2d3ef3cea05fe3
nikuze <nikuzediop@gmail.com>
2023-12-24 10:32:30 +0200

    Removing arrow icon

--- app/components/menu-items/menu-items.tsx
+++ app/components/menu-items/menu-items.tsx
@@ -1,5 +1,5 @@
 export const menuItems = [
-  { id: 1, label: "Bureau d'études", path: "/bureau-etudes" },
+  { id: 1, label: "Bureau Etudes", path: "/bureau-etudes" },
   { id: 2, label: "Produits", path: "/produits" },
   { id: 3, label: "Contact", path: "/contact" },
   { id: 4, label: "Support", path: "/support" },
