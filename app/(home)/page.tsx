e68448c419abb8e094241117371da3b79382956b
nikuze <nikuzediop@gmail.com>
2023-12-24 11:00:24 +0200

    Removing ESLint errors

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -2,7 +2,6 @@
 
 import Layout from "@layout/layout.page";
 import Banner from "./components/banner/banner";
-import BureauSolutions from "./components/bureau-solutions/bureau-solutions";
 import Stats from "./components/stats/stats";
 import Features from "./components/features/feautures";
 
@@ -10,7 +9,6 @@ export default function Home() {
   return (
     <Layout title="Accueil">
       <Banner />
-      {/* <BureauSolutions /> */}
       <Stats />
       <Features />
     </Layout>
