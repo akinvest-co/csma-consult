246414164064f2591dd9b5a010b41deeacb0f7c2
nikuze <nikuzediop@gmail.com>
2023-12-24 10:05:26 +0200

    Inserting content on blog page

--- app/(pages)/blog/page.tsx
+++ app/(pages)/blog/page.tsx
@@ -1,69 +1,40 @@
 "use client";
 
 import Layout from "@app/app/layout/layout.page";
-import { Container, Grid, GridItem } from "@chakra-ui/react";
+import { blogData } from "@app/app/lib/static-data/pages/blog";
+import { Container, Grid, GridItem, Box, Image, Text } from "@chakra-ui/react";
 
 export default function News() {
   return (
     <Layout title="Blog">
-      <Container maxW="container.xl" my="20">
+      <Container maxW="container.lg" my="20">
         <Grid
-          h="550px"
           templateRows="repeat(6, 1fr)"
           templateColumns="repeat(3, 1fr)"
-          gap={4}
-          borderRadius="xl"
-          overflow="hidden"
+          gap={6}
+          h="100%"
         >
-          <GridItem
-            rowSpan={2}
-            colSpan={3}
-            bg="tomato"
-            overflow="hidden"
-            borderRadius="xl"
-          />
-          <GridItem
-            colSpan={1}
-            rowSpan={2}
-            bg="papayawhip"
-            overflow="hidden"
-            borderRadius="xl"
-          />
-          <GridItem
-            colSpan={1}
-            rowSpan={2}
-            bg="tomato"
-            overflow="hidden"
-            borderRadius="xl"
-          />
-          <GridItem
-            colSpan={1}
-            rowSpan={2}
-            bg="papayawhip"
-            overflow="hidden"
-            borderRadius="xl"
-          />
-          <GridItem
-            colSpan={1}
-            rowSpan={2}
-            bg="tomato"
-            overflow="hidden"
-            borderRadius="xl"
-          />
-          <GridItem
-            colSpan={1}
-            rowSpan={2}
-            bg="papayawhip"
-            overflow="hidden"
-            borderRadius="xl"
-          />
-          <GridItem
-            colSpan={1}
-            rowSpan={2}
-            bg="tomato"
-            overflow="hidden"
-            borderRadius="xl"
-          />
+          {blogData.map((blog, index) => (
+            <GridItem
+              key={index}
+              colSpan={index === 0 ? 3 : 1}
+              rowSpan={2}
+              bg={index % 2 === 0 ? "tomato" : "papayawhip"}
+              p="5"
+            >
+              {/* <Image
+                src={blog.image}
+                alt={blog.title}
+                w="100%"
+                h="100%"
+                objectFit="cover"
+              /> */}
+              <Text fontSize="xl" fontWeight="bold">
+                {blog.title}
+              </Text>
+              <Text fontSize="sm">{blog.description}</Text>
+            </GridItem>
+          ))}
         </Grid>
       </Container>
     </Layout>
---
+++ app/lib/static-data/pages/blog/data.ts
@@ -0,0 +1,73 @@
+export const blogData = [
+  {
+    title: "Introduction au QSFP28-100G",
+    image: "lien_de_l_image_1.jpg",
+    description:
+      "Découvrez les caractéristiques et les avantages du module QSFP28-100G. Cette introduction complète vous donnera un aperçu des performances exceptionnelles offertes par cette solution de communication.",
+    authorName: "Auteur 1",
+    date: "01 janvier 2023",
+    authorImage: "lien_de_l_image_auteur_1.jpg",
+    category: "Catégorie 1",
+  },
+  {
+    title: "Mise en œuvre du QSFP28-100G dans les réseaux",
+    image: "lien_de_l_image_2.jpg",
+    description:
+      "Explorez les meilleures pratiques et les étapes clés pour intégrer avec succès le module QSFP28-100G dans votre infrastructure réseau. Ce guide détaillé couvre les aspects techniques et opérationnels.",
+    authorName: "Auteur 2",
+    date: "02 janvier 2023",
+    authorImage: "lien_de_l_image_auteur_2.jpg",
+    category: "Catégorie 2",
+  },
+  {
+    title: "Avantages économiques du QSFP28-100G",
+    image: "lien_de_l_image_3.jpg",
+    description:
+      "Décryptez les avantages économiques et les gains de coûts associés à l'utilisation du module QSFP28-100G. Apprenez comment cette technologie peut améliorer l'efficacité de votre infrastructure tout en réduisant les coûts.",
+    authorName: "Auteur 3",
+    date: "03 janvier 2023",
+    authorImage: "lien_de_l_image_auteur_3.jpg",
+    category: "Catégorie 3",
+  },
+  {
+    title: "Sécurité et fiabilité du QSFP28-100G",
+    image: "lien_de_l_image_4.jpg",
+    description:
+      "Plongez dans les aspects de sécurité et de fiabilité du module QSFP28-100G. Comprenez les protocoles de sécurité et les mesures prises pour assurer une communication fiable dans divers environnements.",
+    authorName: "Auteur 4",
+    date: "04 janvier 2023",
+    authorImage: "lien_de_l_image_auteur_4.jpg",
+    category: "Catégorie 4",
+  },
+  {
+    title: "Innovations futures dans la technologie QSFP28-100G",
+    image: "lien_de_l_image_5.jpg",
+    description:
+      "Explorez les tendances et les développements futurs prévus pour la technologie QSFP28-100G. Découvrez comment cette technologie évoluera pour répondre aux besoins croissants des réseaux de communication modernes.",
+    authorName: "Auteur 5",
+    date: "05 janvier 2023",
+    authorImage: "lien_de_l_image_auteur_5.jpg",
+    category: "Catégorie 5",
+  },
+  {
+    title: "Comparaison des modules QSFP28-100G populaires sur le marché",
+    image: "lien_de_l_image_6.jpg",
+    description:
+      "Évaluez les différents modules QSFP28-100G disponibles sur le marché avec cette comparaison approfondie. Découvrez les spécifications, les performances et les caractéristiques qui distinguent ces produits.",
+    authorName: "Auteur 6",
+    date: "06 janvier 2023",
+    authorImage: "lien_de_l_image_auteur_6.jpg",
+    category: "Catégorie 6",
+  },
+  {
+    title: "Utilisations pratiques du QSFP28-100G dans les centres de données",
+    image: "lien_de_l_image_7.jpg",
+    description:
+      "Découvrez comment intégrer efficacement le module QSFP28-100G dans les centres de données. Explorez les cas d'utilisation pratiques et les bénéfices de cette technologie pour les opérations de centre de données.",
+    authorName: "Auteur 7",
+    date: "07 janvier 2023",
+    authorImage: "lien_de_l_image_auteur_7.jpg",
+    category: "Catégorie 7",
+  },
+  // Ajoutez d'autres blogs au besoin
+];
