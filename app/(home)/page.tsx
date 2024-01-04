1ca85ee13408dd37ff24a307a8915c14b1712da6
Diop-Nikuze <nikuzediop@gmail.com>
2024-01-04 14:56:41 -0800

    Adding products component on home page

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -4,12 +4,14 @@ import Layout from "@layout/layout.page";
 import Banner from "./components/banner/banner";
 import Stats from "./components/stats/stats";
 import Features from "./components/features/feautures";
+import Products from "./components/products/products";
 
 export default function Home() {
   return (
     <Layout title="Accueil">
       <Banner />
       <Stats />
+      <Products />
       <Features />
     </Layout>
   );
