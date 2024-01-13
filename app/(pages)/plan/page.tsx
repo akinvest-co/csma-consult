38c0696b885ec36b609463af3baee575f18a3e2e
Diop-Nikuze <nikuzediop@gmail.com>
2024-01-13 15:23:57 +0200

    Plan du site's page created

---
+++ app/(pages)/plan/page.tsx
@@ -0,0 +1,5 @@
+import PlanSiteView from "@app/app/ui/pages/plan/plan.view"
+
+export default function PlanSite() {
+  return <PlanSiteView />
+}
---
+++ app/ui/pages/plan/plan.view.tsx
@@ -0,0 +1,12 @@
+import Layout from "@app/app/layout/layout.page"
+import { Container, Heading } from "@chakra-ui/react"
+
+export default function PlanSiteView() {
+  return (
+    <Layout>
+      <Container maxW="container.xl">
+        <Heading>Page pour le PLAN DU SITE</Heading>
+      </Container>
+    </Layout>
+  )
+}
