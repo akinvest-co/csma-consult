a4710b8eceac6be3a38c5f0480aa04fb0ee56f47
Diop-Nikuze <nikuzediop@gmail.com>
2024-01-25 15:56:06 +0200

    Making path link rejoignable

--- app/lib/static-data/pages/bureau-etudes/data.ts
+++ app/lib/static-data/pages/bureau-etudes/data.ts
@@ -8,6 +8,7 @@ export const bureauDataIng = [
     descr:
       "Notre expertise se révèle dans la gestion précise et la fiabilité des informations du SIG essentielle pour l'exploitation des réseaux d'opérateurs d'infrastructures télécoms. La mises à jour en temps réel assurent une réactivité constante. Le relevé des infrastructures existantes garantit une compréhension exhaustive pour des interventions efficaces.",
     linkText: "En savoir plus",
+    path: "/etudes-ingénierie",
   },
 ]
 
@@ -19,6 +20,7 @@ export const bureauDataInfr = [
     descr:
       "Pour les Infrastructures Optiques, notre audit évalue avec professionnalisme câbles et boîtes d'épissures. Remontées d'information, plans de boîte stratégiques, et mises à jour SIG basées sur des données terrain assurent une gestion proactive alignée sur les normes les plus strictes, incarnant notre engagement envers l'excellence professionnelle.",
     linkText: "En savoir plus",
+    path: "/audit-infrastructures",
   },
 ]
 
@@ -30,6 +32,7 @@ export const bureauDataRecettes = [
     descr:
       "La Réception commence par une vérification minutieuse de la conformité aux normes en vigueur, suivie de tests et de validations garantissant un fonctionnement optimal. La prise en main des équipements, validée et mise en service en mode isolé, est réalisée avec précision. ",
     linkText: "En savoir plus",
+    path: "/",
   },
 ]
 
@@ -41,5 +44,6 @@ export const bureauDataCentre = [
     descr:
       "La Surveillance de trafic assure un suivi attentif des services en activité et inactifs, avec une réponse rapide aux problèmes signalés. La gestion efficace des équipements et le dépannage à distance garantissent une réactivité, incluant la mobilisation d'équipes en cas de besoin tel qu’une maintenance réseau.",
     linkText: "En savoir plus",
+    path: "/",
   },
 ]
