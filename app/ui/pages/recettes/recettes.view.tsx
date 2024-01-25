7f73174ed7e459fa8c45a8e9d117bb127e3fcbe8
Diop-Nikuze <nikuzediop@gmail.com>
2024-01-25 15:46:23 +0200

    Creating pages for Recettes/Supervision

---
+++ app/ui/pages/recettes/recettes.view.tsx
@@ -0,0 +1,9 @@
+import { Container, Heading } from "@chakra-ui/react"
+
+export default function Recettes() {
+  return (
+    <Container maxW="container.xl">
+      <Heading>Page Recettes et Réception</Heading>
+    </Container>
+  )
+}
---
+++ app/ui/pages/supervision/supervision.view.tsx
@@ -0,0 +1,9 @@
+import { Container, Heading } from "@chakra-ui/react"
+
+export default function Supervision() {
+  return (
+    <Container maxW="container.xl">
+      <Heading>Page Centre de Supervision</Heading>
+    </Container>
+  )
+}
