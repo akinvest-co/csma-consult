d1f49e34bffc103a097425f9624b1610ebeb346d
Diop-Nikuze <nikuzediop@gmail.com>
2024-05-08 20:12:47 +0200

    Single Page for article is done

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -2,18 +2,16 @@ import Layout from "@app/app/layout/layout.page"
 import { BlocksRenderer } from "@strapi/blocks-react-renderer"
 
 import {
-  Box,
   Breadcrumb,
   BreadcrumbItem,
   BreadcrumbLink,
   Container,
-  HStack,
   Heading,
+  SimpleGrid,
   Text,
   VStack,
 } from "@chakra-ui/react"
 import Image from "next/image"
-import NextLink from "next/link"
 import { getArticle } from "@app/app/lib/api/blog/blog"
 
 export default async function article({
@@ -22,12 +20,11 @@ export default async function article({
   params: { slug: string }
 }) {
   const { data: article } = await getArticle(params.slug)
-  console.log(article)
 
   return (
     <Layout>
       <Container maxW="container.lg" py="20">
-        <Breadcrumb>
+        <Breadcrumb color="rgba(89 106 149)" fontSize="md">
           <BreadcrumbItem>
             <BreadcrumbLink href="/blog">Blog</BreadcrumbLink>
           </BreadcrumbItem>
@@ -39,37 +36,48 @@ export default async function article({
           </BreadcrumbItem>
 
           <BreadcrumbItem isCurrentPage>
-            <BreadcrumbLink href="#">{article.attributes.title}</BreadcrumbLink>
+            <BreadcrumbLink>{article.attributes.title}</BreadcrumbLink>
           </BreadcrumbItem>
         </Breadcrumb>
-        <VStack align="start" spacing="8">
-          <Heading lineHeight="shorter"></Heading>
-          <HStack>
-            <Text>{article.attributes.title}</Text>
-            <Text
-              color="grey"
-              suppressHydrationWarning
-              fontSize=".875rem"
-            ></Text>
-          </HStack>
 
-          <Box
-            position="relative"
-            borderRadius="xl"
-            overflow="hidden"
-            // bgImage={`url(${imageUrl})`}
-            bgRepeat="no-repeat"
-            bgSize="cover"
-            bgPos="center"
-            w="100%"
-            h={{ base: "200px", md: "350px" }}
-          >
-            Image
-          </Box>
-        </VStack>
-        {/* <Prose>
-          <Box>{documentToReactComponents(content)}</Box>
-        </Prose> */}
+        <SimpleGrid
+          my="20"
+          columns={{ md: 1, lg: 2 }}
+          alignItems="center"
+          gap="20"
+        >
+          <VStack align="start" spacing="6">
+            <Text
+              fontSize="small"
+              bgColor="#e1f2fd"
+              py="1"
+              px="2"
+              borderRadius="md"
+              color="#0b6999"
+            >
+              {article.attributes.blog_category.data.attributes.name}
+            </Text>
+            <Heading
+              fontSize={{ base: "2.25rem", md: "2.9rem" }}
+              lineHeight={{ base: "2.75rem", md: "3.625rem" }}
+              letterSpacing="-.02em"
+            >
+              {article.attributes.title}
+            </Heading>
+          </VStack>
+
+          <Image
+            src={article.attributes.image.data.attributes.url}
+            width={article.attributes.image.data.attributes.width}
+            height={article.attributes.image.data.attributes.height}
+            alt={article.attributes.title}
+            style={{ borderRadius: "10px" }}
+          />
+        </SimpleGrid>
+
+        <Container>
+          <BlocksRenderer content={article.attributes.body} />
+        </Container>
       </Container>
     </Layout>
   )
