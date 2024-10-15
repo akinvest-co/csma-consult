3255470a582b3edde0a9ebeb5c10b2b573076dd8
BiramaNdiaye <birama.ndiaye@safibre.sn>
2024-10-15 09:33:33 +0000

    test du deploiement automatique

--- app/lib/static-data/home/banner/data.ts
+++ app/lib/static-data/home/banner/data.ts
@@ -3,7 +3,7 @@ import imageBanner from "public/images/bureaux.png"
 export const bannerData = [
   {
     id: 1,
-    title: "Découvrez Nos Expertises et Solutions Avancées",
+    title: "Découvrez nos expertises et solutions avancées",
     intro:
       "L'excellence de notre bureau en télécommunication vous assure les meilleures solutions et produits, garantissant ainsi performance et fiabilité.",
     image: imageBanner,
