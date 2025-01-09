3102cc169c399f889b275d2701b283f6677ade63
BiramaNdiaye <birama.ndiaye@safibre.sn>
2025-01-09 16:59:47 +0000

    modifica

--- app/components/menu-items/menu-items.tsx
+++ app/components/menu-items/menu-items.tsx
@@ -1,10 +1,9 @@
 export const menuItems = [
   { id: 1, label: "Bureau d’études", path: "/bureau-etudes" },
-  { id: 2, label: "Produits", path: "/produits" },
-  { id: 3, label: "Contact", path: "/contact" },
-  { id: 4, label: "Support", path: "/support" },
-  { id: 5, label: "Blog", path: "/blog" },
-  { id: 6, label: "Panier", path: "/cart" },
+  { id: 2, label: "Contact", path: "/contact" },
+  { id: 3, label: "Support", path: "/support" },
+  { id: 4, label: "Blog", path: "/blog" },
+
 ]
 
 export const footerMenu = [
